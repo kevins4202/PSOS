a63754c4ea2e0da8b7b90cb67111016b2081217d
Kevin Song <song_zj@hotmail.com>
2024-07-09 22:46:06 -0500

    counter not working but server is running

--- index.js
+++ index.js
@@ -3,42 +3,51 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-import { CountUp } from './js/countUp.min.js';
+import pkg from 'countup.js';
+const {CountUp} = pkg;
 
 
-window.onload = function() {
+function start() {
   var countUp = new CountUp('target', 1103, {duration: 3});
   countUp.start();
 }
 
 app.use(express.static("public"));
 
+const today = new Date();
+const year = today.getFullYear();
+console.log(year);
 
 app.get("/", (req, res) =>{
-    const today = new Date();
-    const year = today.getFullYear();
-    console.log(year);
     res.render("index.ejs", {
-        y: year
+        currYear: year
     });
 });
 
 app.get("/aboutus", (req, res) =>{
-    res.render("aboutus.ejs")
+    res.render("aboutus.ejs", {
+        currYear: year
+    })
 });
 
 
 app.get("/events", (req, res) =>{
-    res.render("events.ejs")
+    res.render("events.ejs", {
+        currYear: year
+    })
 });
 
 
 app.get("/getinvolved", (req, res) =>{
-    res.render("getinvolved.ejs")
+    res.render("getinvolved.ejs", {
+        currYear: year
+    })
 });
 
 app.get("/blog", (req, res) =>{
-    res.render("blog.ejs")
+    res.render("blog.ejs", {
+        currYear: year
+    })
 });
 
 app.listen(port, () =>{
