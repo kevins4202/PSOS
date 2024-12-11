9ea8093f992bc53d0a305f4e9e95e7ae8fc7a4d4
Kevin Song <song_zj@hotmail.com>
2024-12-11 10:10:24 -0500

    previous commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../index.css';
+
+const Header = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary w-100" style={{ backgroundColor: '#ffcbc4', width: '100vw', margin: 0, padding: 0 }} data-bs-theme="light">
+      <Container fluid style={{ margin: 0, maxWidth: '100%' }}>
+        <div className="navbar-brand">
+          <Navbar.Brand href="/">
+            <img
+              src="/icons/psos_logo.png"
+              style={{ height: '100px', marginRight: '3%' }}
+              alt="PSOS Logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Brand href="/">
+            <span style={{ fontFamily: 'Marmelad, serif', fontSize: '30px' }}>
+              project Save our Skin
+            </span>
+          </Navbar.Brand>
+        </div>
+        <Navbar.Toggle aria-controls="navbarSupportedContent" />
+        <Navbar.Collapse id="navbarSupportedContent">
+          <Nav className="ms-auto">
+            <Nav.Link className="link-dark" href="./index.html">HOME</Nav.Link>
+            <Nav.Link className="link-dark" href="./aboutus.html">ABOUT US</Nav.Link>
+            <Nav.Link className="link-dark" href="./events.html">EVENTS</Nav.Link>
+            <Nav.Link className="link-dark" href="./getinvolved.html">GET INVOLVED</Nav.Link>
+            <Nav.Link className="link-dark" href="./blog.html">BLOG</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
